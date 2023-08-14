e63f266369c2120bd071680149e4b807d10e0aca
Robert Orlando Toro Marquina <roberth.otm@gmail.com>
2023-08-14 16:11:55 -0400

    Se modifico el codigo agregando libreria pandas

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 #Seleccionar columna world population percentage
 
 def run():
-    data = read_csv.read_csv('data.csv') #obtenemos la informacion csv en el formato en forma de diccionario
+    '''
     data=list(filter(lambda item: item['Continent'] == 'South America', data)) #filtramos solo paises de suramerica
-
     countries= list(map(lambda x: x['Country/Territory'], data)) #filtramos con map la columna country para obtener los paises
     percentages= list(map(lambda x: x['World Population Percentage'], data)) #filtramos con map los porcentajes
+    '''
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']     #vamos al dataframe "df" y que solo seleccionaremos los datos "df" donde la columna continente sea igual a suramerica
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+
     charts.generate_pie_chart(countries, percentages) #generamos piechart
 
-
+    data = read_csv.read_csv('data.csv') #obtenemos la informacion csv en el formato en forma de diccionario
     country= input('Type country=> ')
     print(country)
 
